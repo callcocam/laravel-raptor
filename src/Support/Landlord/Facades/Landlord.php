049e8cd829783ef7a49685fb4b1b16af74db75d8
callcocam <7425411+callcocam@users.noreply.github.com>
2025-11-14 21:39:00 +0000

    Fix styling

--- src/Support/Landlord/Facades/Landlord.php
+++ src/Support/Landlord/Facades/Landlord.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
-* Created by Claudio Campos.
-* User: callcocam@gmail.com, contato@sigasmart.com.br
-* https://www.sigasmart.com.br
-*/
+ * Created by Claudio Campos.
+ * User: callcocam@gmail.com, contato@sigasmart.com.br
+ * https://www.sigasmart.com.br
+ */
 
 namespace Callcocam\LaravelRaptor\Support\Landlord\Facades;
 
