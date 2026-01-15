7f12944631ac1c60dc28379c7438bd55da695fff
Claudio Campos <callcocam@gmail.com>
2026-01-15 16:45:05 -0300

    Notificações

--- resources/js/components/NotificationDropdown.vue
+++ resources/js/components/NotificationDropdown.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { useGlobalNotifications } from '@/composables/useGlobalNotifications'
+import { useGlobalNotifications } from '../composables/useGlobalNotifications'
 import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
 import {
